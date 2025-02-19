bd7961d06a908d1e12a38fcdb9eb5477c3aebbc8
Bridget Leary <beleary25@gmail.com>
2025-02-19 10:46:46 -0500

    2/19 updates

--- quiz/server.ts
+++ quiz/server.ts
@@ -70,6 +70,23 @@ app.get('/read/usernames', (req: UserRequest, res: Response) => {
   res.send(usernames);
 });
 
+app.use('/read/username', addMsgToRequest);
+app.get('/read/username/:name', (req: UserRequest, res: Response) => {
+  let name = req.params.name;
+  let users_with_name = req.users?.filter(function(user) {
+    return user.username === name;
+  });
+
+  if (users_with_name?.length === 0) {
+    res.send({
+      error: {message: `${name} not found`, status: 404}
+    });
+  } else {
+    res.send(users_with_name);
+  }
+})
+
+
 // a middleware function that parses the request body to json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
